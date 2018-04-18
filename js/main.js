dea53a05304d8370cbae3eb09734978f6b4ee4e7
ValentinGor <v.gorbatenko@mail.ru>
2018-04-18 20:07:45 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Development: Valentin Gorbatenko
+// E-mail: v.gorbatenko@mail.ru
+// Phone: +38 (066) 178-56-44
+// Site: https://vk.com/gorbatenko1984
+
+//Инициализация owlCarousel
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
