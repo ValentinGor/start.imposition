538f849047f121d8ab8234fea86157aed757f726
Valentin Gorbatenko <v.gorbatenko@mail.ru>
2018-11-22 16:42:27 +0200

    // - Добавлен Normalize.css // - Удалено авторские ссылки // - Устранена ошибка диапазонов media.css

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
-<!--Development: Valentin Gorbatenko-->
-<!--E-mail: v.gorbatenko@mail.ru-->
-<!--Phone: +38 (066) 178-56-44-->
-<!--Site: https://vk.com/gorbatenko1984-->
-
 <!doctype html>
 <html lang="ru">
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Normalize CSS -->
+    <link rel="stylesheet" href="css/normalize.css">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
